7d7b9e727f04b6b8042a0a594e7acb0dea0c6463
Yuri Belebekha <ybelebekha@gmail.com>
2023-01-18 23:30:58 +0200

    Added FeedbackSection component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackSection from "./FeedbackSection";
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    
+    return (
+      <>
+        <FeedbackSection title="Please leave feedback" />
+        
+      </>
+    );
+  }
+}
+
